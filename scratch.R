fa8917bf1e8401fa52ecddddabf68f5ae6dcfef8
slarty101 <slarty101@yahoo.co.uk>
2016-02-23 21:44:45 +0000

    updated scratch

--- scratch.R
+++ scratch.R
@@ -36,6 +36,8 @@ tail(data)
 
 summary(data)
 
+str(data)
+
 ### 3: Start cutting the data down to what we're intested in
 
 newData <- data[ ,c(1,2,5,6,7,8,23,24,25,26,27,28)]
@@ -78,16 +80,6 @@ newData <- filter(newData, FATALITIES > 0 | INJURIES > 0 |  PROPDMG > 0 | CROPDM
 
 
 
-#need to convert PROPDMGEXP to multiplier
-
-#newData_2 <- mutate(newData, prop_mult = If{ newData$PROPDMGEXP == "B" | newData$PROPDMGEXP =="b", 10000000 else if newData$PROPDMGEXP == "M" | newData$PROPDMGEXP =="m", 1000000, else if newData$PROPDMGEXP == "K" | newData$PROPDMGEXP =="k", 1000, else if newData$PROPDMGEXP == "H" | newData$PROPDMGEXP =="h", 100 else 1})
-
-#take 2
-
-#newData$propCost <- ifelse(newData$PROPDMGEXP == "B" | newData$PROPDMGEXP == "b", 10000000,(ifelse(newData$PROPDMGEXP == "M" | newData$PROPDMGEXP == "m", 1000000),(ifelse(newData$PROPDMGEXP == "K" | newData$PROPDMGEXP == "k", 1000),(ifelse(newData$PROPDMGEXP == "H" | newData$PROPDMGEXP == "h", 100),1))))
-
-
-
 #Add a year column
 
 #newData_2 <- mutate(newData, YEAR = year(newData$BGN_DATE))
@@ -95,18 +87,7 @@ newData <- filter(newData, FATALITIES > 0 | INJURIES > 0 |  PROPDMG > 0 | CROPDM
 
 #Fix values for property and crops - take 3
 
-#newData_3 <- mutate(newData, PROPVALUE = paste(newData$PROPDMG, newData$PROPDMGEXP))
 
-
-#PROPCOSTS <- if(newData$PROPDMGEXP == "B" | newData$PROPDMGEXP == "b")
-#                {newData$PROPVALUE<- newData$PROPDMG * 1000000000} 
- #       else if(newData$PROPDMGEXP == "M" | newData$PROPDMGEXP == "m")
-#                {newData$PROPVALUE<- newData$PROPDMG * 1000000} 
-#        else if(newData$PROPDMGEXP == "K" | newData$PROPDMGEXP == "k")
-#                {newData$PROPVALUE<- newData$PROPDMG * 1000} 
-#        else if(newData$PROPDMGEXP == "H" | newData$PROPDMGEXP == "h")
-#                {newData$PROPVALUE<- newData$PROPDMG * 100} 
-#        else{newData$PROPVALUE<- newData$PROPDMG * 1}
 
 #take 92
 #add new column with mutate
@@ -118,13 +99,14 @@ newData <- filter(newData, match(newData$STATE, state.abb))
 
                   #and this if it doesn't work
 
-newData$is_STATE <- mapply(function(st) st %in% state.abb, newData$STATE)
-newData <- newData[newData$is_STATE==TRUE,]
+newData$IS_STATE <- mapply(function(st) st %in% state.abb, newData$STATE)
+newData <- newData[newData$IS_STATE==TRUE,]
 
 #newData$STATE_NAME <- mapply(function(st_name) st_name %in% state.name, newData$STATE) 
 #newData$STATE_NAME <- mapply(function(st_name) st_name %match% newData$STATE,state.abb)
-#state names
-newData$STATE_NAME <- state.name[match(newData$STATE,state.abb)]
+
+#state names for supplementary analysis
+newData$STATENAME <- state.name[match(newData$STATE,state.abb)]
 
 #Justify ignoring data quality issues in PROPDMGEXP and CROPDMGEXP variables
 summary(newData$PROPDMGEXP)
@@ -133,41 +115,43 @@ summary(newData$CROPDMGEXP)
 #newData <- mutate(newData, PROPVALUE= paste(as.numeric(newData$PROPDMG)))
 
 #newData$PROPVALUE = newData$PROPDMG
-calcCOST <- function(dmg, dmg_exp) dmg * switch(to_upper(dmg_exp), H=100, K=1000, M=1000000, B=1000000000, 1)
-newData$PROPCOSTS <- mapply(calcCOST, newData$PROPDMG, newData$PROPDMGEXP)
-newData$CROPCOSTS <- mapply(calcCOST, newData$CROPDMG, newData$CROPDMGEXP)
+CalcCost <- function(dmg, dmg_exp) dmg * switch(toupper(dmg_exp), H=100, K=1000, M=1000000, B=1000000000, 1)
+newData$PROPCOSTS <- mapply(CalcCost, newData$PROPDMG, newData$PROPDMGEXP)
+newData$CROPCOSTS <- mapply(CalcCost, newData$CROPDMG, newData$CROPDMGEXP)
 
-##
-#PROPCOSTS <- if(newData$PROPDMGEXP == "B"){newData$PROPVALUE<- newData$PROPDMG * 1000000000} else
-#                 if(newData$PROPDMGEXP == "b"){newData$PROPVALUE<- newData$PROPDMG * 1000000000} else
-#                 if(newData$PROPDMGEXP == "M"){newData$PROPVALUE<- newData$PROPDMG * 1000000} else
-#                 if(newData$PROPDMGEXP == "m"){newData$PROPVALUE<- newData$PROPDMG * 1000000} else
-#                 if(newData$PROPDMGEXP == "K"){newData$PROPVALUE<- newData$PROPDMG * 1000} else
-#                 if(newData$PROPDMGEXP == "k"){newData$PROPVALUE<- newData$PROPDMG * 1000} else
-#                 if(newData$PROPDMGEXP == "H"){newData$PROPVALUE<- newData$PROPDMG * 100} else
-#                 if(newData$PROPDMGEXP == "h"){newData$PROPVALUE<- newData$PROPDMG * 100} else
-#                {newData$PROPVALUE<- newData$PROPDMG * 1}
-
-#PROPCOSTS <- if(newData$PROPDMGEXP == "B" | newData$PROPDMGEXP == "b"){newData$PROPVALUE<- newData$PROPDMG * 1000000000} else if(newData$PROPDMGEXP == "M" | newData$PROPDMGEXP == "m"){newData$PROPVALUE<- newData$PROPDMG * 1000000} else if(newData$PROPDMGEXP == "K" | newData$PROPDMGEXP == "k"){newData$PROPVALUE<- newData$PROPDMG * 1000} else if(newData$PROPDMGEXP == "H" | newData$PROPDMGEXP == "h"){newData$PROPVALUE<- newData$PROPDMG * 100} else{newData$PROPVALUE<- newData$PROPDMG * 1}
-
-#CROPCOSTS <- if(newData$CROPDMGEXP == "B" | newData$CROPDMGEXP == "b")
-#                {newData$CROPVALUE<- newData$CROPDMG * 1000000000} 
-#        else if(newData$CROPDMGEXP == "M" | newData$CROPDMGEXP == "m")
-#                {newData$CROPVALUE<- newData$CROPDMG * 1000000} 
-#        else if(newData$CROPDMGEXP == "K" | newData$CROPDMGEXP == "k")
-#                {newData$CROPVALUE<- newData$CROPDMG * 1000} 
-#        else if(newData$CROPDMGEXP == "H" | newData$CROPDMGEXP == "h")
-#                {newData$CROPVALUE<- newData$CROPDMG * 100} 
-#        else{newData$CROPVALUE<- newData$CROPDMG * 1}
-
-#propCost <- ifelse(newData_3$PROPDMGEXP == "B", 1000000000,
-#                   ifelse(newData_3$PROPDMGEXP == "b", 1000000000)
-#                   ifelse(newData_3$PROPDMGEXP == "M", 1000000)
-#                   ifelse(newData_3$PROPDMGEXP == "m", 1000000)
-#                   ifelse(newData_3$PROPDMGEXP == "K", 1000)
-#                   ifelse(newData_3$PROPDMGEXP == "k", 1000)
-#                   ifelse(newData_3$PROPDMGEXP == "H", 100)
-#                   ifelse(newData_3$PROPDMGEXP == "h", 100)1)
+# Group the 985 different event types in the dataset into a more manageable number
+GrpEvents <- function(wEvent) {
+        wEvent <- tolower(wEvent)
+        ifelse(grepl("lightning", wEvent), "lightning",
+        ifelse(grepl("hail|rain|wet|precipitation", wEvent), "precipitation",
+        ifelse(grepl("flood|fld", wEvent), "flood",
+        ifelse(grepl("snow|winter|wintry|blizzard|sleet|cold|ice|freeze|avalanche|icy", wEvent), "winter",
+        ifelse(grepl("thunder|tstm|tornado|wind|hurricane|funnel|tropical +storm", wEvent), "storm",
+        ifelse(grepl("fire", wEvent), "fire",
+        ifelse(grepl("fog|visibility|dark|dust", wEvent), "visibility",
+        ifelse(grepl("surf|surge|tide|tsunami|current|seiche", wEvent), "tidal",
+        ifelse(grepl("heat|high +temp|record +temp|warm|dry|drought", wEvent), "heat",
+        ifelse(grepl("volcan|landslide", wEvent), "seismic activity",
+        "Other"))))))))))
+        }
+newData$WEATHERGROUP <- mapply(GrpEvents, newData$EVTYPE)
 
 
-#^disnae work either
+
+#supplementary maps
+storm_health <- mutate(newData, HEALTH = (newData$FATALITIES + newData$INJURIES))
+sum_health <- storm_health %>% group_by(tolower(STATE_NAME)) %>% summarise(HEALTH=sum(HEALTH))
+colnames(sum_health) <- c("region", "value")
+state_choropleth(sum_health)
+
+storm_costs <- mutate(newData, TOTCOST = (newData$PROPCOSTS + newData$CROPCOSTS))
+sum_costs <- storm_costs %>% group_by(tolower(STATE_NAME)) %>% summarise(TOTCOST=sum(TOTCOST))
+colnames(sum_costs) <- c("region", "value")
+state_choropleth(sum_costs)
+
+sum_fatal <- newData %>% group_by(tolower(STATE_NAME)) %>% summarise(FATALITIES=sum(FATALITIES))
+colnames(sum_fatal) <- c("region", "value")
+state_choropleth(sum_fatal)
+
+
+
